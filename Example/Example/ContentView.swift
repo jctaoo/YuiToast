0a975bf8cb25b5ffe7c45b22b815dae44aa65fc1
jctaoo <jctaoo@outlook.com>
2020-08-11 10:15:33 +0800

    Setup Example Project

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by jctaoo on 2020/8/11.
+//  Copyright © 2020 jctaoo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
